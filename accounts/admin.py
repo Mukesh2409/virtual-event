98833b063bf5d6f8a9ead92332f18026b78fce70
Mukesh Kadiyam <104091448+Mukesh2409@users.noreply.github.com>
2025-04-13 13:08:01 +0530

    Delete accounts directory

--- accounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
-
-# Register the User model with the custom admin interface
-admin.site.register(User, UserAdmin)
